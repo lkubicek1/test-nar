fd9f20d010ecef495f136a6ae56fad11e70754cb
Lee Kubicek <lkubicek1@gmail.com>
2023-11-02 16:40:10 -0400

    Update checkConnections.sh

--- checkConnections.sh
+++ checkConnections.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-LOGFILE="/tmp/connections.log"
+LOGFILE="/tmp/recon/connections.log"
 
 while true; do
     OF_COUNT=$(ps -u "$(whoami)" -o pid= | xargs -L 1 sh -c 'lsof -p "$1" 2>/dev/null | wc -l' | awk '{sum += $1} END {print sum}')
     NT_COUNT=$(ss -ant | grep -v State | wc -l)
-    echo "$(date) | LSOF: ${OF_COUNT} | CONNECTIONS: ${NT_COUNT}" >> "${LOGFILE}"
+    msg="$(date) | LSOF: ${OF_COUNT} | CONNECTIONS: ${NT_COUNT}"
+    echo "$msg"       # This will print the message to stdout.
+    echo "$msg" >> "$LOGFILE"  # This will append the message to the log file.
     sleep 10
 done
